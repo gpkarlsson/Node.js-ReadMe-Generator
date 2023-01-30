5dea89b9d0ce380549c34778d4a3684c21f63a90
Gordon K <114494147+gpkarlsson@users.noreply.github.com>
2023-01-30 17:33:19 -0600

    Update index.js
    trying to make sources match for git

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const MarkDown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 
 //Application questions
 const questions = [
@@ -54,12 +54,7 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
-
-// TODO: Create a function to initialize app
-
-// Function call to initialize app
-//init();
+//function writeToFile(fileName, data) { }
 
 
 function runQuery() {
